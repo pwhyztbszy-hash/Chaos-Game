b0b1e6d49620eb804d174992ee91116ff172e73d
pwhyztbszy-hash <243625237+pwhyztbszy-hash@users.noreply.github.com>
2025-11-18 17:14:56 -0800

    Disable font loading and instructions in ChaosGame
    Commented out font loading and instructions display code.

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -15,6 +15,20 @@ int main()
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game!!", Style::Default);
+
+	/*sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+	{
+    cout << "ERROR: Could not load arial.ttf" << endl;
+    return 1;
+	}
+
+	sf::Text instructions;
+	instructions.setFont(font);
+	instructions.setCharacterSize(24);
+	instructions.setFillColor(sf::Color::White);
+	instructions.setPosition(10, 10);
+	instructions.setString("Click 3 points to place triangle vertices");*/
 	
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
