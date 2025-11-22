940eb579498b886a6dee045ff715d2b4361ca341
pwhyztbszy-hash <243625237+pwhyztbszy-hash@users.noreply.github.com>
2025-11-21 21:13:02 -0800

    Improve rendering of vertices and points
    Refactor drawing logic to separate rectangle and dot rendering.

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -115,17 +115,36 @@ int main()
         ****************************************
         */
         window.clear();
-		for (int i = 0; i < vertices.size(); i++)
-		{
-			RectangleShape rect(Vector2f(10, 10));
-			rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-			rect.setFillColor(Color::Blue);
-			window.draw(rect);
-		}
-		///TODO:  Draw points
-		Vertex line[] = { Vertex(lastPoint), Vertex(newPoint) };
+		 for(int i = 0; i < vertices.size(); i++)
+		 {
 
-		window.draw(line, 2, Lines);
-		window.display();
-    }
-}
+		 RectangleShape rect(Vector2f(10,10));
+	
+		 rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+	
+		 rect.setFillColor(Color::Blue);
+	
+		 window.draw(rect);
+	
+		 }
+		
+		 ///TODO:  Draw points
+		for(int i = 0; i < vertices.size(); i++)
+		 {
+
+		 RectangleShape dot(Vector2f(10,10));
+	
+		 dot.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+	
+		 dot.setFillColor(Color::White);
+	
+		 window.draw(dot);
+	
+		 }
+		
+		
+		 window.display();
+		
+		 }
+		
+		}
