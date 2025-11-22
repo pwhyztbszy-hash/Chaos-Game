29c9da669c9c62802bf8976c5e2b5adbfa50c702
pwhyztbszy-hash <243625237+pwhyztbszy-hash@users.noreply.github.com>
2025-11-22 09:57:23 -0800

    update the new code

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cmath>
 
 //Make the code easier to type with "using namespace"
 using namespace sf;
@@ -26,18 +27,27 @@ int main()
     Text instructions;
     instructions.setFont(font);
     instructions.setCharacterSize(24);
-    instructions.setFillColor(sf::Color::White);
+    instructions.setFillColor(Color::White);
     instructions.setPosition(10, 10);
-    instructions.setString("Click 3 points to place triangle vertices");
+    instructions.setString("Click at least 3 points to place vertices, press enter when done");
 
     vector<Vector2f> vertices;
     vector<Vector2f> points;
+
+    Vector2f lastVertex;
+    Vector2f randVertex;
+
     Vector2f lastPoint;
-    Vector2f randPoint;
     Vector2f newPoint;
+
+    bool selectingVertices = true;
+    bool generatingPoints = true;
+    const float pi = 3.14159;
+    float optimalRatio = 0.5;
 
     while (window.isOpen())
     {
+        window.setKeyRepeatEnabled(false);
 
         /*
         ****************************************
@@ -53,7 +63,7 @@ int main()
                 window.close();
             }
 
-            if (event.type == sf::Event::MouseButtonPressed)
+            if (event.type == Event::MouseButtonPressed)
             {
                 if (event.mouseButton.button == sf::Mouse::Left)
                 {
@@ -61,29 +71,63 @@ int main()
                     cout << "mouse x: " << event.mouseButton.x << std::endl;
                     cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-                    if (vertices.size() < 3)
+                    if (selectingVertices)
                     {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-
-                        if (vertices.size() == 3)
-                        {
-                            instructions.setString("Click a 4th point to start Chaos Game");
-                        }
                     }
                     else if (points.size() == 0)
                     {
                         ///fourth click
                         ///push back to points vector
                         points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-                        instructions.setString("Generating Chaos Game...");
                     }
                 }
             }
-        }
 
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-        {
-            window.close();
+            if (event.type == Event::KeyPressed)
+            {
+                //handle exit with escape
+                if (event.key.code == Keyboard::Escape)
+                {
+                    window.close();
+                }
+
+                //finish choosing vertices, calculate optimal ratio
+                if (event.key.code == Keyboard::Enter)
+                {
+                    if (selectingVertices && vertices.size() >= 3)
+                    {
+                        selectingVertices = false;
+
+                        if (vertices.size() % 4 == 0)
+                        {
+                            optimalRatio = 1 / (1 + tan(pi / vertices.size()));
+                        }
+
+                        else if (vertices.size() % 4 == 1 || vertices.size() % 4 == 3)
+                        {
+                            optimalRatio = 1 / (1 + (2 * sin(pi / (2 * vertices.size()))));
+                        }
+                        else if (vertices.size() % 4 == 2) {
+                            optimalRatio = 1 / (1 + sin(pi / vertices.size()));
+                        }
+                        cout << "optimal ratio with " << vertices.size() << " sides: " << optimalRatio << endl;
+
+                        instructions.setString("Click another point to start Chaos Game");
+                    }
+                }
+            }
+            if (event.type == Event::KeyReleased)
+            {
+                //pause generation
+                if (event.key.code == Keyboard::Space && points.size() != 0)
+                {
+
+                    generatingPoints = !generatingPoints;
+                    cout << "generatingPoints now " << generatingPoints << endl;
+                    instructions.setString("Paused Generation...");
+                }
+            }
         }
 
         /*
@@ -91,23 +135,31 @@ int main()
         Update
         ****************************************
         */
-        if (points.size() > 0)
+        if (points.size() > 0 && generatingPoints)
         {
             ///generate more point(s)
             ///select random vertex
             ///calculate midpoint between random vertex and the last point in the vector
             ///push back the newly generated coord.
+            /// 
+            instructions.setString("Generating Chaos Game...");
+
+            do    //no same vertex twice in a row when more than 3 vertices
+            {
+                randVertex = vertices[rand() % vertices.size()];
+            } while (randVertex == lastVertex && vertices.size() > 3);
 
             lastPoint = points[points.size() - 1];
 
-            randPoint = vertices[rand() % 3];
-
+            //new point = fraction of the distance between new and old point
             newPoint = {
-                (lastPoint.x + randPoint.x) / 2,
-                (lastPoint.y + randPoint.y) / 2
+                (lastPoint.x - (lastPoint.x - randVertex.x) * optimalRatio),
+                (lastPoint.y - (lastPoint.y - randVertex.y) * optimalRatio)
             };
 
             points.push_back(newPoint);
+
+            lastVertex = randVertex;
         }
 
         /*
@@ -117,8 +169,10 @@ int main()
         */
         window.clear();
 
-        if (vertices.size() == 3)
+        //draw outline
+        if (!selectingVertices)
         {
+
             ConvexShape shape;
             shape.setPointCount(vertices.size());
             shape.setFillColor(Color::Transparent);
@@ -130,8 +184,10 @@ int main()
             }
 
             window.draw(shape);
+
         }
 
+        //draw vertices points
         for (size_t i = 0; i < vertices.size(); i++)
         {
 
@@ -147,7 +203,7 @@ int main()
         }
         
 
-        ///TODO:  Draw points
+        //draw points
         for (size_t i = 0; i < points.size(); i++)
         {
 
@@ -161,7 +217,6 @@ int main()
             window.draw(dot);
 
         }
-		
         window.draw(instructions);
 
         window.display();
